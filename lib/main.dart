fb34536827f3adf61f8f14859d2a1ba913334843
krishna-incrypto <vpkrishnanand@gmail.com>
2020-01-12 12:41:13 +0530

    title change

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Expense Manager'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
